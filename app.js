d8dbd8fb43c0ab87ae28933fcfa29048f4a08567
Ninja1O1 <manojpangi@gmail.com>
2024-08-06 00:09:57 +0530

    Added the Map view

--- app.js
+++ app.js
@@ -10,8 +10,12 @@ const io = socket(server);
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 
+io.on("connection", function(socket){
+    console.log("a new connection");
+})
+
 app.get("/", function(req, res) {
-    res.send("Hello World!");
+    res.render("index");
 });
 
 server.listen(3000, () => {
